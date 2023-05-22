1b56596f8133b85d4cc4de4d34efd7222b70fba8
Jordy Yeoman <jordy.yeoman@entaingroup.com.au>
2023-05-22 13:13:50 +0800

    chore: basics of python

--- test/start.py
+++ test/start.py
@@ -1,3 +1,34 @@
 # This program prints Hello, world!
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
 
-print('Hello, world!')
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+# Note: The self parameter is a reference to the current instance of the class, 
+# and is used to access variables that belong to the class.
+class BigDog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("'Woof' - " + self.name)
+    
+    def getBreed(self):
+        print("Dog breed is: " + self.breed)
+
+doggo = BigDog("Layla", "Maremma")
+doggo.bark()
+doggo.getBreed()
